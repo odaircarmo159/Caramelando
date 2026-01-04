978b3ebdbd58541e975c6a617cf952927d14e2fe
Odair Carmo <odair.carmo096@academico.ufgd.edu.br>
2026-01-03 20:44:26 -0400

    primeira estrutura do sistema

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>🐶 Caramelando</h1>
+        <p>
+          Encontre um amigo para a vida toda 💛
+        </p>
+  
+        <button>
+          Ver animais para adoção
+        </button>
+      </div>
+    )
+  }
+  
+  export default Home
+  
